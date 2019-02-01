456000e75561dfe78f9ee6d9fb4e0e0a713610c8
James <iamjimmyjimmy@yahoo.com>
2019-01-31 17:51:50 -0800

    Imported Buefy and added NavBar
    Added navbar UI

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <navbar/>
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import navbar from './components/Navbar.vue'
+  export default {
+    name: 'App',
+    components:{
+      navbar:navbar
+    }
+  }
 </script>
 
 <style>
@@ -18,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Homepage.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        BUG HOUND
+    </div>
+</template>
