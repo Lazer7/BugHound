cbab9b45be7c195a9dfdb3786239d60e4de44b13
James <iamjimmyjimmy@yahoo.com>
2019-02-20 00:28:12 -0800

    Added Advance Search Feature

---
+++ src/components/modals/AdvancedSearchModal.vue
@@ -0,0 +1,226 @@
+<template>
+  <div class="modal is-active">
+    <div class="modal-background"/>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title has-text-left">
+          <strong>Advance Search</strong>
+        </p>
+      </header>
+      <section class="modal-card-body">
+        <div class="has-text-left">
+          <b-field label="Program">
+            <b-select placeholder="Select Program" v-model="program">
+              <option
+                v-for="(level,index) in ProgramData"
+                :value="level"
+                :key="index"
+              >{{ level.name }}</option>
+            </b-select>
+          </b-field>
+
+          <b-field label="Report Type">
+            <b-select placeholder="Select Report Type" v-model="report_type">
+              <option v-for="data in ReportData" :value="data.key" :key="data.key">{{ data.value }}</option>
+            </b-select>
+          </b-field>
+
+          <b-field label="Severity">
+            <b-select placeholder="Select Severity" v-model="severity">
+              <option
+                v-for="data in SeverityData"
+                :value="data.key"
+                :key="data.key"
+              >{{ data.value }}</option>
+            </b-select>
+          </b-field>
+
+          <b-field label="Date Reported">
+            <b-datepicker
+              placeholder="Type or select a date..."
+              v-model="date_reported"
+              icon="calendar"
+              editable
+            ></b-datepicker>
+          </b-field>
+
+          <hr>
+          <div class="columns">
+            <div class="column is-3">
+              <b-field label="Functional Area">
+                <b-select placeholder="Functional Area" v-model="area">
+                  <option
+                    v-for="(data,index) in AreaData"
+                    :value="data"
+                    :key="index"
+                  >{{ data.name }}</option>
+                </b-select>
+              </b-field>
+            </div>
+            <div class="column is-3">
+              <b-field label="Assigned To">
+                <b-select placeholder="Assigned To" v-model="assigned_to">
+                  <option
+                    v-for="(data,index) in EmployeeData"
+                    :value="data"
+                    :key="index"
+                  >{{ data.firstname }} {{data.lastname}}</option>
+                </b-select>
+              </b-field>
+            </div>
+            <div class="column is-3">
+              <b-field label="Status">
+                <b-select placeholder="Status" v-model="status">
+                  <option
+                    v-for="data in StatusData"
+                    :value="data.key"
+                    :key="data.key"
+                  >{{ data.value }}</option>
+                </b-select>
+              </b-field>
+            </div>
+          </div>
+
+          <div class="columns">
+            <div class="column is-3">
+              <b-field label="Priority">
+                <b-select placeholder="Priority" v-model="priority">
+                  <option v-for="data in PriorityData" :value="data" :key="data">{{ data }}</option>
+                </b-select>
+              </b-field>
+            </div>
+            <div class="column is-3">
+              <b-field label="Resolution">
+                <b-select placeholder="Resolution" v-model="resolution">
+                  <option
+                    v-for="data in ResolutionData"
+                    :value="data.key"
+                    :key="data.key"
+                  >{{ data.value }}</option>
+                </b-select>
+              </b-field>
+            </div>
+            <div class="column is-3">
+              <b-field label="Resolved By">
+                <b-select placeholder="Resolved By" v-model="resolved_by">
+                  <option
+                    v-for="(data,index) in EmployeeData"
+                    :value="data"
+                    :key="index"
+                  >{{ data.firstname }} {{data.lastname}}</option>
+                </b-select>
+              </b-field>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer class="modal-card-foot">
+        <button class="button" @click="submit">Submit</button>
+        <button class="button" @click="close">Cancel</button>
+      </footer>
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  name: "AdvanceSearch",
+  data() {
+    return {
+      ProgramData: [],
+      EmployeeData: [],
+      ReportData: [
+        { key: 1, value: "Coding Error" },
+        { key: 2, value: "Design Issue" },
+        { key: 3, value: "Suggestion" },
+        { key: 4, value: "Documentation" },
+        { key: 5, value: "Hardware" },
+        { key: 6, value: "Query" }
+      ],
+      SeverityData: [
+        { key: 1, value: "Fatal" },
+        { key: 2, value: "Serious" },
+        { key: 3, value: "Minor" }
+      ],
+      AreaData: [],
+      StatusData: [{ key: 1, value: "Open" }, { key: 2, value: "Close" }],
+      PriorityData: [1, 2, 3, 4, 5],
+      ResolutionData: [
+        { key: 1, value: "Pending" },
+        { key: 2, value: "Fixed" },
+        { key: 3, value: "Irreproducible" },
+        { key: 4, value: "Deferred" },
+        { key: 5, value: "At designed" },
+        { key: 6, value: "Can't be fixed" },
+        { key: 7, value: "Withdrwawn by reporter" },
+        { key: 8, value: "Needs more information" },
+        { key: 9, value: "Disagree with suggestion" },
+        { key: 10, value: "Duplicate" }
+      ],
+      program: undefined,
+      report_type: undefined,
+      severity: undefined,
+      date_reported: undefined,
+      area: undefined,
+      assigned_to: undefined,
+      status: undefined,
+      priority: undefined,
+      resolution: undefined,
+      resolved_by: undefined
+    };
+  },
+  mounted() {
+    var self = this;
+    axios.get(this.$store.getters["routes/getProgram"]).then(result => {
+      self.ProgramData = result.data.programs;
+      axios.get(this.$store.getters["routes/getEmployees"]).then(result => {
+        self.EmployeeData = result.data.employees;
+        axios.get(this.$store.getters["routes/getArea"]).then(result => {
+          self.AreaData = result.data.areas;
+        });
+      });
+    });
+  },
+  methods: {
+    close() {
+      this.$emit("toggle",{});
+    },
+    submit() {
+      var programName = this.program !==undefined? this.program.name: undefined;
+      var areaName = this.area !==undefined? this.area.name: undefined;
+      var resolvedName = this.resolved_by!== undefined? this.resolved_by.id:undefined;
+      var assignedName = this.assigned_to!==undefined?this.assigned_to:undefined;
+
+      var searchData={
+        program: programName,
+        reporttype: this.report_type,
+        severity: this.severity,
+        datereported: this.date_reported,
+        area: areaName,
+        assignedto: assignedName,
+        status: this.status,
+        priority: this.priority,
+        resolution: this.resolution,
+        resolvedby: resolvedName
+      }
+      this.$emit("toggle",searchData);
+    }
+  }
+};
+</script>
+<style scoped>
+.image {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 20%;
+}
+.button {
+  background: #2d2d2d;
+  color: #ffffff;
+}
+.button:hover {
+  background: #ffffff;
+  color: #2d2d2d;
+}
+</style>
--- src/components/pages/CompanyBugList.vue
+++ src/components/pages/CompanyBugList.vue
@@ -1,184 +1,220 @@
 <template>
   <div class="section has-text-left">
-    <img width="30%" src="../../assets/companybug.png"/>
+    <img width="30%" src="../../assets/companybug.png">
     <div class="container notification">
       <div class="columns">
         <div class="column is-10">
           <b-field label="Search">
-            <b-input v-model="search" @keydown.native.enter="updateList"></b-input>
+            <b-input
+              placeholder="Press Enter to search..."
+              v-model="search"
+              @keydown.native.enter="updateList"
+            ></b-input>
           </b-field>
         </div>
         <div class="column is-2">
-            <b-field label="Search Field">
-              <b-select placeholder="Select Field" v-model=field>
-                  <option
-                      v-for="option in fieldData"
-                      :value="option.key"
-                      :key="option.key">
-                      {{ option.value }}
-                  </option>
-              </b-select>
+          <b-field label="Search Field">
+            <b-select placeholder="Select Field" v-model="field">
+              <option
+                v-for="option in fieldData"
+                :value="option.key"
+                :key="option.key"
+              >{{ option.value }}</option>
+            </b-select>
           </b-field>
         </div>
       </div>
+      <button class="button" @click="advance=true">Advance Search</button>
     </div>
     <div class="container">
-    <div class="box" v-for="(bug,index) in currentBuglist" :key=index>
-      <bugbox :data="bug" :employeeList="employees"/>
+      <div class="box" v-for="(bug,index) in currentBuglist" :key="index">
+        <bugbox :data="bug" :employeeList="employees"/>
+      </div>
     </div>
-    </div>
+    <advance v-if="advance" v-on:toggle="advanceSearch"/>
   </div>
 </template>
 <script>
-import bugbox from '../media/BugBox';
-import axios from 'axios';
-  export default {
-    name: "companybuglist",
-    components:{
-      bugbox:bugbox
-    },
-    data(){
-      return{
-        buglist:[],
-        currentBuglist:[],
-        employees:[],
-        search:"",
-        field:undefined,
-        fieldData:[
-          {key: "programid"     ,value:"Program"},
-          {key: "reporttype"   ,value:"Report Type"},
-          {key: "severity"     ,value:"Severity"},
-          {key: "area"         ,value:"Functional Area"},
-          {key: "assignedto"   ,value:"Assigned To"},
-          {key: "status"       ,value:"Status"},
-          {key: "priority"     ,value:"Priority"},
-          {key: "resolution"   ,value:"Resolution"},
-          {key: "reportedby"   ,value:"Reported By"},
-          {key: "datereported" ,value:"Report Date"},
-          {key: "resolvedby"   ,value:"Resolved By"}
-        ]
-      }
-    },
-    mounted(){
-      var self = this;
-      axios.get(this.$store.getters['routes/getBugs']).then(result=>{
-        self.buglist = result.data.bugs;
-        self.currentBuglist = self.buglist;
-      })
-      axios.get(this.$store.getters['routes/getEmployees']).then(result=>{
-        self.employees = result.data.employees;
-      })
-    },
-    methods:{
-      updateList(){
-        if(this.search!=="" && this.field!==undefined){
-          if(this.field==="datereported"){this.currentBuglist = this.searchDate(this.search)}
-          else if(this.field.match("by")){this.currentBuglist = this.searchEmployee(this.search)}
-          else if(this.field.match("severity")){this.currentBuglist =this.searchSeverity(this.search)}
-          else if(this.field.match("status")){this.currentBuglist =this.searchStatus(this.search)}
-          else if(this.field.match("reporttype")){this.currentBuglist =this.searchReportType(this.search)}
-          else if(this.field.match("resolution")){this.currentBuglist =this.searchResolution(this.search)}
-          else{this.currentBuglist = this.searchOther(this.search)}
-        } else {
-          this.currentBuglist = this.buglist;
+import bugbox from "../media/BugBox";
+import advance from "../modals/AdvancedSearchModal";
+import axios from "axios";
+export default {
+  name: "companybuglist",
+  components: {
+    bugbox: bugbox,
+    advance: advance
+  },
+  data() {
+    return {
+      buglist: [],
+      currentBuglist: [],
+      employees: [],
+      search: "",
+      field: undefined,
+      fieldData: [
+        { key: "programid", value: "Program" },
+        { key: "reporttype", value: "Report Type" },
+        { key: "severity", value: "Severity" },
+        { key: "area", value: "Functional Area" },
+        { key: "assignedto", value: "Assigned To" },
+        { key: "status", value: "Status" },
+        { key: "priority", value: "Priority" },
+        { key: "resolution", value: "Resolution" },
+        { key: "reportedby", value: "Reported By" },
+        { key: "datereported", value: "Report Date" },
+        { key: "resolvedby", value: "Resolved By" }
+      ],
+      advance: false
+    };
+  },
+  mounted() {
+    var self = this;
+    axios.get(this.$store.getters["routes/getBugs"]).then(result => {
+      self.buglist = result.data.bugs;
+      self.currentBuglist = self.buglist;
+    });
+    axios.get(this.$store.getters["routes/getEmployees"]).then(result => {
+      self.employees = result.data.employees;
+    });
+  },
+  methods: {
+    advanceSearch(value){
+      this.advance=false;
+      var final = [];
+      for (var property in value) {
+        if (value.hasOwnProperty(property)) {
+          if(value[property] !==undefined){
+            this.search = value[property];
+            this.field  = property;
+            this.updateList();
+            final = final.concat(this.currentBuglist);
+          }
         }
-      },
-      searchDate(value){
-        var result = [];
-        var date = new Date(value);
-        this.buglist.forEach(bug =>{
-          var currentDate = new Date(bug[this.field]);
-          if(currentDate !== undefined &&
-             currentDate.getFullYear() === date.getFullYear() &&
-             currentDate.getMonth() === date.getMonth() &&
-             currentDate.getDate() === date.getDate()
-          ){
-            result.push(bug);
-          }
-        });
-        return result;
-      },
-      searchEmployee(value){
-        var result = [];
-        this.buglist.forEach(bug=>{
-          this.employees.forEach(employee=>{
-            if(bug[this.field]===employee.id &&
-               (value.match(employee.firstname)  ||
-                value.match(employee.lastname)   ||
-                value.match(employee.username)   
-                )
-            ){
-              result.push(bug);
-            }
-          })
-        })
-        return result;
-      },
-      searchSeverity(value){
-        var result = [];
-        var data =["Fatal","Serious","Minor"];
-        this.buglist.forEach(bug=>{
-          if(value.match(data[bug[this.field]-1])) result.push(bug);
-        });
-        return result;
-      },
-      searchStatus(value){
-        var result = [];
-        var data = ["Open","Close"];
-        this.buglist.forEach(bug=>{
-          if(value.match(data[bug[this.field]-1])) result.push(bug);
-        })
-        return result;
-      },
-      searchReportType(value){
-        var result = [];
-        var data = [
-                "Coding Error",
-                "Design Issue",
-                "Suggestion",
-                "Documentation",
-                "Hardware",
-                "Query"
-                ];
-        this.buglist.forEach(bug=>{
-          if(value.match(data[bug[this.field]-1])) result.push(bug);
-        })
-        return result;
-      },
-      searchResolution(value){
-        var result = [];
-        var data = [
-              "Pending",
-              "Fixed",
-              "Irreproducible",
-              "Deferred",
-              "At designed",
-              "Can't be fixed",
-              "Withdrwawn by reporter",
-              "Needs more information",
-              "Disagree with suggestion",
-              "Duplicate"
-            ];
-        this.buglist.forEach(bug=>{
-          if(value.match(data[bug[this.field]-1])) result.push(bug);
-        })
-        return result;
-      },
-      searchOther(value){
-        var result = [];
-        this.buglist.forEach(bug=>{
-          if(value.match(bug[this.field])){
-            result.push(bug);
-          }
-        })
-        return result;
       }
+      this.currentBuglist = final;
+      this.field = undefined;
+      this.search= undefined;
+    },
+    updateList() {
+      if (this.search !== "" && this.field !== undefined) {
+        if (this.field === "datereported") {
+          this.currentBuglist = this.searchDate(this.search);
+        } else if (this.field.match("by")) {
+          this.currentBuglist = this.searchEmployee(this.search);
+        } else if (this.field.match("severity")) {
+          this.currentBuglist = this.searchSeverity(this.search);
+        } else if (this.field.match("status")) {
+          this.currentBuglist = this.searchStatus(this.search);
+        } else if (this.field.match("reporttype")) {
+          this.currentBuglist = this.searchReportType(this.search);
+        } else if (this.field.match("resolution")) {
+          this.currentBuglist = this.searchResolution(this.search);
+        } else {
+          console.log(this.field);
+          console.log(this.search);
+          this.currentBuglist = this.searchOther(this.search);
+        }
+      } else {
+        this.currentBuglist = this.buglist;
+      }
+    },
+    searchDate(value) {
+      var result = [];
+      var date = new Date(value);
+      this.buglist.forEach(bug => {
+        var currentDate = new Date(bug[this.field]);
+        if (
+          currentDate !== undefined &&
+          currentDate.getFullYear() === date.getFullYear() &&
+          currentDate.getMonth() === date.getMonth() &&
+          currentDate.getDate() === date.getDate()
+        ) {
+          result.push(bug);
+        }
+      });
+      return result;
+    },
+    searchEmployee(value) {
+      var result = [];
+      this.buglist.forEach(bug => {
+        this.employees.forEach(employee => {
+          if (
+            bug[this.field] === employee.id &&
+            (value.match(employee.firstname) ||
+              value.match(employee.lastname) ||
+              value.match(employee.username))
+          ) {
+            result.push(bug);
+          }
+        });
+      });
+      return result;
+    },
+    searchSeverity(value) {
+      var result = [];
+      var data = ["Fatal", "Serious", "Minor"];
+      this.buglist.forEach(bug => {
+        if (value.match(data[bug[this.field] - 1])) result.push(bug);
+      });
+      return result;
+    },
+    searchStatus(value) {
+      var result = [];
+      var data = ["Open", "Close"];
+      this.buglist.forEach(bug => {
+        if (value.match(data[bug[this.field] - 1])) result.push(bug);
+      });
+      return result;
+    },
+    searchReportType(value) {
+      var result = [];
+      var data = [
+        "Coding Error",
+        "Design Issue",
+        "Suggestion",
+        "Documentation",
+        "Hardware",
+        "Query"
+      ];
+      this.buglist.forEach(bug => {
+        if (value.match(data[bug[this.field] - 1])) result.push(bug);
+      });
+      return result;
+    },
+    searchResolution(value) {
+      var result = [];
+      var data = [
+        "Pending",
+        "Fixed",
+        "Irreproducible",
+        "Deferred",
+        "At designed",
+        "Can't be fixed",
+        "Withdrwawn by reporter",
+        "Needs more information",
+        "Disagree with suggestion",
+        "Duplicate"
+      ];
+      this.buglist.forEach(bug => {
+        if (value.match(data[bug[this.field] - 1])) result.push(bug);
+      });
+      return result;
+    },
+    searchOther(value) {
+      var result = [];
+      this.buglist.forEach(bug => {
+        if (value.match(bug[this.field])) {
+          result.push(bug);
+        }
+      });
+      return result;
     }
   }
+};
 </script>
 
 <style scoped>
-.company{
+.company {
   margin: 5% 5% 5% 0%;
 }
 </style>
