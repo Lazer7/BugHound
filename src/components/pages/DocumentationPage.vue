795e6276f823089b688f3ba1843f1795b127230e
James <iamjimmyjimmy@yahoo.com>
2019-02-26 01:55:56 -0800

    Added more documentation

--- src/components/pages/DocumentationPage.vue
+++ src/components/pages/DocumentationPage.vue
@@ -2,6 +2,10 @@
   <div>
     <div class="hero is-fullheight">
         <img src="https://i.imgur.com/6FiTa0a.png"/>
+        <img src="../../assets/documentation.png"/>
+        <img src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/52706392_1233491446827148_8883884408596070400_n.jpg?_nc_cat=106&_nc_ht=scontent-lax3-1.xx&oh=792858361452d59c88075364687a0f73&oe=5CDA105C"/>
+        <img src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/52553560_1213622412123883_164793050424410112_n.jpg?_nc_cat=104&_nc_ht=scontent-lax3-1.xx&oh=07c2082947c0921efae16cb1ccbe10d8&oe=5D1C09A6"/>
+        <img src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/52306244_10157138872948669_8352855940608819200_n.jpg?_nc_cat=103&_nc_ht=scontent-lax3-1.xx&oh=516d8239781c75d920252ef6e7696110&oe=5D1EC2C8"/>
     </div>
   </div>
 </template>
